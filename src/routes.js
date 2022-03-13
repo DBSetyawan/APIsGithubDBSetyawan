58d57484487179946055ef367ac3fa3e9c9d1baf
daniel <artexsdns@gmail.com>
2022-03-13 08:28:22 +0700

    Add routes api

--- src/routes.js
+++ src/routes.js
@@ -1,4 +1,8 @@
 import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
+import Main from './pages/Main/Main';
+import Repository from './pages/Repository/Repository';
 
 export default function Routes() {
 }
