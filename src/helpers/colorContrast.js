3d884ab5d5efd2d1b64690d2bf6e263560f8fd7b
daniel <artexsdns@gmail.com>
2022-03-13 08:49:25 +0700

    Hex color contrast

---
+++ src/helpers/colorContrast.js
@@ -0,0 +1,13 @@
+export default function colorContrast(color) {
+    let hex = color;
+    if (hex.indexOf('#') === 0) {
+      hex = hex.slice(1);
+    }
+    // convert 3-digit hex to 6-digits.
+    if (hex.length === 3) {
+      hex = hex[0] + hex[0] + hex[1] + hex[1] + hex[2] + hex[2];
+    }
+    if (hex.length !== 6) {
+      throw new Error('Invalid HEX color.');
+    }
+  }
