ee5d551319aabe4d58b4e010107da62c463f2ee2
daniel <artexsdns@gmail.com>
2022-03-13 08:50:25 +0700

    Hex Set R G B

--- src/helpers/colorContrast.js
+++ src/helpers/colorContrast.js
@@ -1,13 +1,16 @@
 export default function colorContrast(color) {
-    let hex = color;
-    if (hex.indexOf('#') === 0) {
-      hex = hex.slice(1);
-    }
-    // convert 3-digit hex to 6-digits.
-    if (hex.length === 3) {
-      hex = hex[0] + hex[0] + hex[1] + hex[1] + hex[2] + hex[2];
-    }
-    if (hex.length !== 6) {
-      throw new Error('Invalid HEX color.');
-    }
+  let hex = color;
+  if (hex.indexOf('#') === 0) {
+    hex = hex.slice(1);
   }
+  if (hex.length === 3) {
+    hex = hex[0] + hex[0] + hex[1] + hex[1] + hex[2] + hex[2];
+  }
+  if (hex.length !== 6) {
+    throw new Error('Invalid HEX color.');
+  }
+  const r = parseInt(hex.slice(0, 2), 16);
+  const g = parseInt(hex.slice(2, 4), 16);
+  const b = parseInt(hex.slice(4, 6), 16);
+  return r * 0.299 + g * 0.587 + b * 0.114 > 186 ? '#333' : '#fff';
+}
