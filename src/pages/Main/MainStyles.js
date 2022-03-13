55a7d6e57e474c868066434cd7461768444a5114
daniel <artexsdns@gmail.com>
2022-03-13 07:53:15 +0700

    Add Styles List Repository

--- src/pages/Main/MainStyles.js
+++ src/pages/Main/MainStyles.js
@@ -41,16 +41,16 @@ export const SubmitButton = styled.button.attrs(props => ({
 
   
 export const Form = styled.form`
-margin-top: 30px;
-display: flex;
-flex-direction: row;
-input {
-  flex: 1;
-  border: solid ${props => (props.error ? '2px #e41111' : '1px #eee')};
-  padding: 10px 15px;
-  border-radius: 4px;
-  font-size: 16px;
-}
+    margin-top: 30px;
+    display: flex;
+    flex-direction: row;
+    input {
+    flex: 1;
+    border: solid ${props => (props.error ? '2px #e41111' : '1px #eee')};
+    padding: 10px 15px;
+    border-radius: 4px;
+    font-size: 16px;
+    }
 `;
 
 const rotate = keyframes`
@@ -60,4 +60,44 @@ transform: rotate(0deg)
 to {
 transform: rotate(360deg)
 }
+`;
+
+export const List = styled.ul`
+  margin-top: 30px;
+  list-style-type: none;
+  font-size: 16px;
+  li {
+    padding: 15px 0;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+    & + li {
+      border-top: 1px solid #eee;
+    }
+    img {
+      width: 32px;
+      margin-right: 12px;
+      border-radius: 50%;
+      border: 2px solid #dbdbdb;
+    }
+    a {
+      display: flex;
+      align-items: center;
+      color: inherit;
+      text-decoration: none;
+      &:hover {
+        color: #7159c1;
+      }
+    }
+    button {
+      color: #999;
+      background: none;
+      border: 0;
+      padding: 6px 0 6px 16px;
+      &:hover {
+        color: #7159c1;
+      }
+    }
+  }
 `;
