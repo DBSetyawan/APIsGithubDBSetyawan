e7a121fcc56661a331d2033023a67eb19c60dabf
daniel <artexsdns@gmail.com>
2022-03-13 08:59:39 +0700

    remove List

--- src/pages/Main/MainStyles.js
+++ src/pages/Main/MainStyles.js
@@ -88,49 +88,8 @@ export const List = styled.ul`
   }
 `;
 
-
 export const ErrorMessage = styled.span`
   display: block;
   margin-top: 5px;
   color: #e41111;
-`;
-
-export const List = styled.ul`
-  margin-top: 30px;
-  list-style-type: none;
-  font-size: 16px;
-  li {
-    padding: 15px 0;
-    display: flex;
-    flex-direction: row;
-    justify-content: space-between;
-    align-items: center;
-    & + li {
-      border-top: 1px solid #eee;
-    }
-    img {
-      width: 32px;
-      margin-right: 12px;
-      border-radius: 50%;
-      border: 2px solid #dbdbdb;
-    }
-    a {
-      display: flex;
-      align-items: center;
-      color: inherit;
-      text-decoration: none;
-      &:hover {
-        color: #7159c1;
-      }
-    }
-    button {
-      color: #999;
-      background: none;
-      border: 0;
-      padding: 6px 0 6px 16px;
-      &:hover {
-        color: #7159c1;
-      }
-    }
-  }
 `;
