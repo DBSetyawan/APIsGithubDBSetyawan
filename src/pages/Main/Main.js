a9aaf3c053d727312ffe312fc66a83631482ff29
daniel <artexsdns@gmail.com>
2022-03-13 07:56:13 +0700

    Add State for Repository Account DBSetyawan

--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -2,3 +2,24 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 
 import Container, { Icon } from '../../components/Container';
+
+
+class Main extends Component {
+
+    state = {
+        newRepo: '',
+        repositories: [
+          {
+            name: 'DBSetyawan/3PS-OpenAPI-Open-Source',
+            owner: {
+              name: 'facebook',
+              avatar_url: 'https://avatars.githubusercontent.com/u/31942002?v=4',
+            },
+          },
+        ],
+        loading: false,
+        error: false,
+        errorMessage: '',
+      };
+
+}
